f918b9cb54a9f2c2c9d5d963ddd0675a38823f8e
lhk0721 <dlgusrb6546@naver.com>
2026-03-14 21:26:15 +0900

    feat: split item routes into controller router

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-from typing import Union
+from controller import items
 
 app = FastAPI()
+app.include_router(items.router)
 
 @app.get("/")
 def read_root():
     return{"Hello":"world"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
